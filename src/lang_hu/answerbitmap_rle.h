a44c63bcdeaf7065a9054d645c690e390ceecb1f
bbbbbr <bbbbbr@users.noreply.github.com>
2025-02-12 02:14:18 -0800

    Larger hungarian answer list

--- src/lang_hu/answerbitmap_rle.h
+++ src/lang_hu/answerbitmap_rle.h
@@ -1,7 +1,7 @@
 
 
-#define rle_answerBitmapIndex_sz_comp 165
-#define rle_answerBitmapIndex_sz_decomp 229
+#define rle_answerBitmapIndex_sz_comp 251
+#define rle_answerBitmapIndex_sz_decomp 247
 
 
 extern const unsigned char rle_answerBitmapIndex[];
