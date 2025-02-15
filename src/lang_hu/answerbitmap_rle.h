9d240f51504226e944cc937fe8f02340dd546380
bbbbbr <bbbbbr@users.noreply.github.com>
2025-02-14 18:49:20 -0800

    Lang: Remove some words from hu dictionary (MKCK1)

--- src/lang_hu/answerbitmap_rle.h
+++ src/lang_hu/answerbitmap_rle.h
@@ -1,7 +1,7 @@
 
 
-#define rle_answerBitmapIndex_sz_comp 251
-#define rle_answerBitmapIndex_sz_decomp 247
+#define rle_answerBitmapIndex_sz_comp 249
+#define rle_answerBitmapIndex_sz_decomp 246
 
 
 extern const unsigned char rle_answerBitmapIndex[];
